1d09b35309b5cc5b786fed4863c877c3c754d098
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-03-20 13:01:49 +0100

    Add files via upload

---
+++ model.js
@@ -0,0 +1,61 @@
+let model = {
+    currentView: null,
+    screen: {
+        login: {
+            email: null,
+            password: null
+        },
+        home: {
+            searchBar: null,
+            feed: null,
+            friendList: null,
+            eventFeed: null,
+        },
+        searchBar: {
+            peopleFeed: null,
+            searchBar: null,
+            eventFeed: null,
+            filter: null,
+        },
+        eventOrganizer: {
+            calendar: null,
+        },
+        profile: {
+            profileImage: null,
+            name: null,
+            followers: null,
+            following: null,
+            previousEvents: null,
+            upcomingEvents: null,
+            eventFeed: null,
+            level: null
+        },
+        createAccount: {
+            name: null,
+            email: null,
+            password1: null,
+            password2: null,
+            age: null,
+            hobbies: null,
+            categories: null,
+            bio: null,
+        },
+        settings: {
+            style: null,
+            language: null,
+            userStatistics: [],
+            privacyPreferences: null,
+        },
+        levelOverview: {
+            exp: null,
+            unlocks: []
+        }
+    }
+}
+
+
+
+
+
+
+
